f0a95eda726fbd7dd7d39a7bfa97a17112bf790b
Aditya Hari <aditya.hari.ft21@mail.umy.ac.id>
2022-12-14 15:51:04 +0700

    menambah tampilan menu

--- Queues_044/Program.cs
+++ Queues_044/Program.cs
@@ -97,7 +97,27 @@ namespace Queues_019
         }
         static void Main(string[] args)
         {
-            
+            Queues queue = new Queues();
+            char ch;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1. Implement insert operation");
+                    Console.WriteLine("2. Implement delete operation");
+                    Console.WriteLine("3. Display values");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("\nEnter your choice: ");
+                    ch = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine();
+                    
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Check for the values entered.");
+                }
+            }
         }
     }
 }
